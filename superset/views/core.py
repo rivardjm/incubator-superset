ba4e385ca8bbab14e1b553de3e6bc8ec8658dbf1
Erik Ritter <erik.ritter@airbnb.com>
2019-10-10 13:57:14 -0700

    [Config] Cache static resources

--- superset/views/core.py
+++ superset/views/core.py
@@ -3127,8 +3127,17 @@ appbuilder.add_separator("Sources")
 @app.after_request
 def apply_http_headers(response):
     """Applies the configuration's http headers to all responses"""
-    for k, v in config.get("HTTP_HEADERS").items():
+
+    # HTTP_HEADERS is deprecated, this provides backwards compatibility
+    for k, v in (
+        config.get("OVERRIDE_HTTP_HEADERS", {}).items()
+        | config.get("HTTP_HEADERS", {}).items()
+    ):
         response.headers[k] = v
+
+    for k, v in config.get("DEFAULT_HTTP_HEADERS", {}).items():
+        if k not in response.headers:
+            response.headers[k] = v
     return response
 
 
